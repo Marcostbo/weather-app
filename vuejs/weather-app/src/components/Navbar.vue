81160e49e2dabaa37d7d82e256392a3280fc69e2
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-08-07 21:33:56 -0300

    Set up navbar with vuejs

---
+++ vuejs/weather-app/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <head>
+        <meta charset="utf-8">
+        <title> Weather App | Cupcake </title>
+    </head>
+    <h1 class="navbar"> Weather Dashboard </h1>
+</template>
+
+<style scope>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+}
+body {
+  background: #E3F2FD;
+}
+.navbar {
+    background: #03c04a;
+    font-size: 1.75rem;
+    text-align: center;
+    padding: 18px 0;
+    color: #fff;
+    margin: 0;
+}
+</style>
